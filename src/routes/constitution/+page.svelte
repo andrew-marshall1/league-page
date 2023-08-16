89a34c2419349bcacaae241f24b66d99fbfa2947
Andy-Marshall <141158142+Andy-Marshall@users.noreply.github.com>
2023-08-16 10:09:30 -0600

    Update +page.svelte

--- src/routes/constitution/+page.svelte
+++ src/routes/constitution/+page.svelte
@@ -256,7 +256,7 @@
     <h2 class="sectionHeading" bind:this={seven}>Section 7 League Finances</h2>
     
     <h3 bind:this={sevenOne}>7.1 League Dues</h3>
-    <p>League dues are set at ${dues}. Dues are collected through <a href="https://www.venmo.com/u/Andy-Marshall-17">Venmo</a> and collection will be sent out no later than the first of August. All managers must be fully paid up before the start of regular season. In the event that a manager fails to pay before the deadline, a 10% penalty will be addd to their payment and will go towards the payouts at the end of the year.</p>
+    <p>League dues are set at ${dues}. Dues are collected through <a href="https://www.venmo.com/u/Andy-Marshall-17">Venmo</a> and collection will be sent out prior to the date of the draft. All managers must be fully paid up before the start of the draft. In the event that a manager fails to pay before the deadline, a 10% penalty will be addd to their payment and will go towards the payouts at the end of the year and the commisioner reserves the right to select the first pick for the offending manager.</p>
     
     <h3 bind:this={sevenTwo}>7.2 Payout</h3>
     <p>League payout is structured as follows:</p>
